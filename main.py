16296e679f1aa3202c7740e44d1ab38c4c545735
alchemist-of-andoris <the.alchemist.of.andoris@gmail.com>
2025-04-02 21:10:42 -0500

    Set up main pythong file and constants python file

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {str(SCREEN_WIDTH)}")
+    print(f"Screen height: {str(SCREEN_HEIGHT)}")
+
+if __name__ == "__main__":
+    main()
