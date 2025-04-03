8780e0959e78f0794f487b4bbd0c0611e1dc7d62
alchemist-of-andoris <the.alchemist.of.andoris@gmail.com>
2025-04-03 16:28:01 -0500

    Set up game loop with screen creation and updated the .gitignore file.

--- main.py
+++ main.py
@@ -2,11 +2,22 @@
 
 import pygame
 from constants import *
+import time
 
 def main():
+    pygame.init()
+
     print("Starting Asteroids!")
     print(f"Screen width: {str(SCREEN_WIDTH)}")
     print(f"Screen height: {str(SCREEN_HEIGHT)}")
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
