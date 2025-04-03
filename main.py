1998413e112d797acbb518afcb8e2b546aa530c2
alchemist-of-andoris <the.alchemist.of.andoris@gmail.com>
2025-04-03 17:02:33 -0500

    added a game clock to manage framerate

--- main.py
+++ main.py
@@ -2,15 +2,15 @@
 
 import pygame
 from constants import *
-import time
 
 def main():
-    pygame.init()
-
     print("Starting Asteroids!")
     print(f"Screen width: {str(SCREEN_WIDTH)}")
     print(f"Screen height: {str(SCREEN_HEIGHT)}")
-    
+
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     while(True):
         for event in pygame.event.get():
@@ -18,6 +18,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        
 
 if __name__ == "__main__":
     main()
